fb197a0e60ea22c28478442b683d1b655122edcb
Sascha Deeg <sascha.deeg@gmail.com>
2019-08-05 22:39:37 +0200

    Erweiterung

---
+++ src/main/java/EnableBlabbermouth.java
@@ -0,0 +1,13 @@
+import de.youthclubstage.blabbermouth.core.CoreConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+@Import(CoreConfiguration.class)
+@Configuration
+public @interface EnableBlabbermouth {
+}
